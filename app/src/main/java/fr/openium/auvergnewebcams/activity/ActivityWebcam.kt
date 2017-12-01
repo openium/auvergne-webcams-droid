ae8cac420c1e79ca076684d31750c64856300c61
laura <lriviere>
2017-12-01 17:36:59 +0100

    Auvergne WebCam Android refs #23338 Si l'orientation du téléphone sur le détail d'une webcam est paysage alors on cache la toolbar

--- app/src/main/java/fr/openium/auvergnewebcams/activity/ActivityWebcam.kt
+++ app/src/main/java/fr/openium/auvergnewebcams/activity/ActivityWebcam.kt
@@ -1,19 +1,39 @@
 package fr.openium.auvergnewebcams.activity
 
+import android.content.res.Configuration
+import android.os.Bundle
 import android.support.v4.app.Fragment
 import fr.openium.auvergnewebcams.Constants
+import fr.openium.auvergnewebcams.ext.gone
+import fr.openium.auvergnewebcams.ext.show
 import fr.openium.auvergnewebcams.fragment.FragmentWebcam
 import fr.openium.auvergnewebcams.fragment.FragmentWebcamVideo
 import fr.openium.auvergnewebcams.model.Webcam
+import kotlinx.android.synthetic.main.toolbar.*
 
 /**
  * Created by t.coulange on 09/12/2016.
  */
 class ActivityWebcam : AbstractActivityFragment() {
 
+    private var typeWebcam: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        typeWebcam = intent?.getStringExtra(Constants.KEY_TYPE)
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration?) {
+        super.onConfigurationChanged(newConfig)
+        if(newConfig?.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            toolbar.show()
+        } else {
+            toolbar.gone()
+        }
+    }
+
     override fun getDefaultFragment(): Fragment? {
-        val type = intent?.getStringExtra(Constants.KEY_TYPE)
-        if (type == Webcam.WEBCAM_TYPE.VIEWSURF.nameType) {
+        if (typeWebcam == Webcam.WEBCAM_TYPE.VIEWSURF.nameType) {
             return FragmentWebcamVideo()
         } else {
             return FragmentWebcam()
