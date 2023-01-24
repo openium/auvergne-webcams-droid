585570c24166b7553833893b7a6ac84dcd9f3b8f
Ludovic Godart <l.godart@openium.fr>
2023-01-24 12:18:40 +0100

    Fix .arr import

--- app/src/main/java/fr/openium/auvergnewebcams/base/AbstractDialog.kt
+++ app/src/main/java/fr/openium/auvergnewebcams/base/AbstractDialog.kt
@@ -1,18 +1,12 @@
 package fr.openium.auvergnewebcams.base
 
 import androidx.appcompat.app.AppCompatDialogFragment
-import fr.openium.auvergnewebcams.utils.DateUtils
-import fr.openium.auvergnewebcams.utils.PreferencesUtils
 import io.reactivex.disposables.CompositeDisposable
-import org.koin.android.ext.android.inject
 
 /**
  * Created by Openium on 19/02/2019.
  */
 abstract class AbstractDialog : AppCompatDialogFragment() {
-
-    protected val prefUtils by inject<PreferencesUtils>()
-    protected val dateUtils by inject<DateUtils>()
 
     protected val disposables: CompositeDisposable = CompositeDisposable()
 
