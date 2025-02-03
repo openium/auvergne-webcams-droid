6573136c5c0329cdf83c25d6491ec5ea8806d8d8
Lucas <l.zborowski@openium.fr>
2025-02-03 18:04:34 +0100

    ajout animation splashscreen (sans nuages)

--- app/src/main/java/fr/openium/auvergnewebcams/ui/splash/ActivitySplash.kt
+++ app/src/main/java/fr/openium/auvergnewebcams/ui/splash/ActivitySplash.kt
@@ -1,88 +1,14 @@
 package fr.openium.auvergnewebcams.ui.splash
 
 import android.os.Bundle
-import android.view.animation.AnimationUtils
-import androidx.lifecycle.ViewModelProvider
-import fr.openium.auvergnewebcams.R
-import fr.openium.auvergnewebcams.base.AbstractActivity
-import fr.openium.auvergnewebcams.ui.main.ActivityMain
-import fr.openium.kotlintools.ext.show
-import fr.openium.kotlintools.ext.startActivity
-import io.reactivex.Completable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.rxkotlin.addTo
-import kotlinx.android.synthetic.main.activity_splash.linearLayoutSplashText
-import kotlinx.android.synthetic.main.activity_splash.mCloud1
-import kotlinx.android.synthetic.main.activity_splash.mCloud2
-import kotlinx.android.synthetic.main.activity_splash.mCloud3
-import kotlinx.android.synthetic.main.activity_splash.mCloud4
-import kotlinx.android.synthetic.main.activity_splash.mCloud5
-import kotlinx.android.synthetic.main.activity_splash.mCloud6
-import kotlinx.android.synthetic.main.activity_splash.mCloud7
-import timber.log.Timber
-import java.util.concurrent.TimeUnit
+import androidx.fragment.app.Fragment
+import fr.openium.auvergnewebcams.base.AbstractActivityFragment
 
-class ActivitySplash : AbstractActivity() {
-
-    override val layoutId: Int = R.layout.activity_splash
-
-    private lateinit var viewModelSplash: ViewModelSplash
-
-    // --- Life cycle
-    // ---------------------------------------------------
+class ActivitySplash : AbstractActivityFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        viewModelSplash = ViewModelProvider(this).get(ViewModelSplash::class.java)
-
-        // Get new data
-        Completable.merge(
-            listOf(
-                viewModelSplash.updateData(),
-                Completable.timer(500, TimeUnit.MILLISECONDS)
-                    .observeOn(AndroidSchedulers.mainThread()).doOnComplete {
-                        linearLayoutSplashText.show()
-                        mCloud1.show()
-                        mCloud2.show()
-                        mCloud3.show()
-                        mCloud4.show()
-                        mCloud5.show()
-                        mCloud6.show()
-                        mCloud7.show()
-
-                        val alphaAnimation =
-                            AnimationUtils.loadAnimation(this, R.anim.anim_alpha_only)
-                        mCloud5.startAnimation(alphaAnimation)
-                        mCloud6.startAnimation(alphaAnimation)
-                        mCloud7.startAnimation(alphaAnimation)
-
-                        val translateRightSlowAnimation =
-                            AnimationUtils.loadAnimation(this, R.anim.anim_translate_right_slow)
-                        mCloud1.startAnimation(translateRightSlowAnimation)
-
-                        val translateLeftSlowAnimation =
-                            AnimationUtils.loadAnimation(this, R.anim.anim_translate_left_slow)
-                        mCloud2.startAnimation(translateLeftSlowAnimation)
-
-                        val translateRightFastAnimation =
-                            AnimationUtils.loadAnimation(this, R.anim.anim_translate_right_fast)
-                        mCloud3.startAnimation(translateRightFastAnimation)
-
-                        val translateLeftFastAnimation =
-                            AnimationUtils.loadAnimation(this, R.anim.anim_translate_left_fast)
-                        mCloud4.startAnimation(translateLeftFastAnimation)
-                    })
-        ).subscribe({
-            startActivityMain()
-        }, { Timber.e(it) }).addTo(disposables)
     }
 
-    // --- Methods
-    // ---------------------------------------------------
-
-    private fun startActivityMain() {
-        startActivity<ActivityMain>()
-        finish()
-    }
+    override fun getDefaultFragment(): Fragment = FragmentSplash()
 }
---
+++ app/src/main/java/fr/openium/auvergnewebcams/ui/splash/components/SplashText.kt
@@ -0,0 +1,58 @@
+package fr.openium.auvergnewebcams.ui.splash.components
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import fr.openium.auvergnewebcams.R
+
+@Composable
+fun SplashText(isVisible: Boolean = false) {
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = fadeIn(animationSpec = androidx.compose.animation.core.tween(600)),
+        exit = fadeOut(animationSpec = androidx.compose.animation.core.tween(300))
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .padding(top = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.splash_loading_cameras),
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                modifier = Modifier
+                    .padding(10.dp),
+                textAlign = TextAlign.Center,
+                color = Color.White
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            CircularProgressIndicator(
+                modifier = Modifier.size(40.dp),
+                color = Color.White
+            )
+        }
+    }
+}
