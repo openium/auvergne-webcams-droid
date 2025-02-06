a0687d8e3deb94a0925cabc1368655ab25b9560e
Lucas <l.zborowski@openium.fr>
2025-02-06 14:48:41 +0100

    fix/ ajout de la super animation des nuages

---
+++ app/src/main/java/fr/openium/auvergnewebcams/ui/splash/components/CloudWithAnimation.kt
@@ -0,0 +1,56 @@
+package fr.openium.auvergnewebcams.ui.splash.components
+
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.absoluteOffset
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import fr.openium.auvergnewebcams.R
+
+@Composable
+fun CloudWithAnimation(
+    cloud: Painter = painterResource(R.drawable.ic_splash_cloud1),
+    initialOffsetX: Dp = 0.dp,
+    initialOffsetY: Dp = 0.dp,
+    animatedOffsetX: Dp = 0.dp,
+    size: Dp = 32.32.dp,
+    animationAlphaDuration: Int = 1500,
+    animationOffsetDuration: Int = 2000
+) {
+    var startAnimation by remember { mutableStateOf(false) }
+    LaunchedEffect(Unit) {
+        startAnimation = true
+    }
+    val alphaAnim by animateFloatAsState(
+        targetValue = if (startAnimation) 1f else 0f,
+        animationSpec = tween(durationMillis = animationAlphaDuration)
+    )
+    val offsetXAnim by animateDpAsState(
+        targetValue = if (startAnimation) animatedOffsetX else 0.dp,
+        animationSpec = tween(durationMillis = animationOffsetDuration)
+    )
+
+    Image(
+        painter = cloud,
+        contentDescription = "Cloud",
+        contentScale = ContentScale.Fit,
+        modifier = Modifier
+            .size(size)
+            .absoluteOffset(x = initialOffsetX + offsetXAnim, y = initialOffsetY)
+            .alpha(alphaAnim)
+    )
+}
--- app/src/main/java/fr/openium/auvergnewebcams/ui/splash/components/SplashScreen.kt
+++ app/src/main/java/fr/openium/auvergnewebcams/ui/splash/components/SplashScreen.kt
@@ -48,13 +48,63 @@ fun SplashScreen(vm: ViewModelSplash = koinViewModel(), startActivityMain: () ->
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.align(Alignment.Center)
         ) {
-            Image(
-                painter = painterResource(id = R.drawable.ic_splash),
-                contentDescription = "Splash Image",
-                modifier = Modifier
-                    .size(250.dp),
-                contentScale = ContentScale.Fit
-            )
+            Box(modifier = Modifier.size(250.dp)) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_splash),
+                    contentDescription = "Splash Image",
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Fit
+                )
+                CloudWithAnimation(
+                    cloud = painterResource(R.drawable.ic_splash_cloud1),
+                    initialOffsetX = 55.dp,
+                    initialOffsetY = 67.27.dp,
+                    animatedOffsetX = (15).dp,
+                    size = 50.50.dp
+                )
+                CloudWithAnimation(
+                    cloud = painterResource(R.drawable.ic_splash_cloud2),
+                    initialOffsetX = 150.dp,
+                    initialOffsetY = 67.27.dp,
+                    animatedOffsetX = (-15).dp,
+                    size = 50.50.dp
+                )
+                CloudWithAnimation(
+                    cloud = painterResource(R.drawable.ic_splash_cloud3),
+                    initialOffsetX = 40.dp,
+                    initialOffsetY = 67.27.dp,
+                    animatedOffsetX = (15).dp,
+                    size = 32.32.dp
+                )
+                CloudWithAnimation(
+                    cloud = painterResource(R.drawable.ic_splash_cloud4),
+                    initialOffsetX = 185.dp,
+                    initialOffsetY = 67.27.dp,
+                    animatedOffsetX = (-15).dp,
+                    size = 32.32.dp
+                )
+                CloudWithAnimation(
+                    cloud = painterResource(R.drawable.ic_splash_cloud5),
+                    initialOffsetX = 40.dp,
+                    initialOffsetY = 40.dp,
+                    animatedOffsetX = 0.dp,
+                    size = 40.40.dp,
+                )
+                CloudWithAnimation(
+                    cloud = painterResource(R.drawable.ic_splash_cloud6),
+                    initialOffsetX = 100.dp,
+                    initialOffsetY = 10.0.dp,
+                    animatedOffsetX = 0.dp,
+                    size = 50.50.dp,
+                )
+                CloudWithAnimation(
+                    cloud = painterResource(R.drawable.ic_splash_cloud7),
+                    initialOffsetX = 191.dp,
+                    initialOffsetY = 35.0.dp,
+                    animatedOffsetX = 0.dp,
+                    size = 40.40.dp,
+                )
+            }
             SplashText(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -62,6 +112,5 @@ fun SplashScreen(vm: ViewModelSplash = koinViewModel(), startActivityMain: () ->
                     .padding(top = 20.dp)
             )
         }
-
     }
 }
